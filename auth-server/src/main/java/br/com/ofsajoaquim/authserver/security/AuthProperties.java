6f4f17ecb624ad5c582598487a53f1b156977831
Luiz Felipe <luiz.sa.joaquim@gmail.com>
2022-07-31 08:45:41 -0300

    Configuração chave
    	* Criado arquivo awserver.jks com as chaves privadas e publicas
    	* Criado os atributos da classe interna JksProperties
    	* Adcionado as anatações personalidadas do arquivo jks dentro do
    application.properties

--- auth-server/src/main/java/br/com/ofsajoaquim/authserver/security/AuthProperties.java
+++ auth-server/src/main/java/br/com/ofsajoaquim/authserver/security/AuthProperties.java
@@ -6,22 +6,38 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Component
 @Validated
 @ConfigurationProperties
+@Getter
+@Setter
 public class AuthProperties {
 	
 	@NotBlank
 	private String providerUri;
 
-	public String getProviderUri() {
-		return providerUri;
-	}
-
-	public void setProviderUri(String providerUri) {
-		this.providerUri = providerUri;
-	}
+	@NotBlank
+	private JksProperties jks;
 	
 	
+	@Getter
+	@Setter
+	static class JksProperties {
+		
+		@NotBlank
+		private String keypass;
+		
+		@NotBlank
+		private String storepass;
+		
+		@NotBlank
+		private String alias;
+		
+		@NotBlank
+		private String path;
+	}
 	
 }
