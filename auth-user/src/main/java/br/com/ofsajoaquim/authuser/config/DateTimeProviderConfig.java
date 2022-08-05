c590b5d8a3bd91744fda6b8e674c6537a015344f
Luiz Felipe <luiz.sa.joaquim@gmail.com>
2022-08-04 22:11:32 -0300

    User config
     * Adicionado CorConfig
     * Adicionado DateTimeProvider
     * Adicionad FirstuserConfig

---
+++ auth-user/src/main/java/br/com/ofsajoaquim/authuser/config/DateTimeProviderConfig.java
@@ -0,0 +1,19 @@
+package br.com.ofsajoaquim.authuser.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+@Configuration
+public class DateTimeProviderConfig {
+
+	@Bean(name = "auditingDateTimeProvider")
+	public DateTimeProvider dateTimeProvider() {
+		return () -> Optional.of(OffsetDateTime.now());
+	}
+	
+}
