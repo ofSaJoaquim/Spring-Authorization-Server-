ab8da9865d025c975dffb1975a9da07879c6629e
Luiz Felipe <luiz.sa.joaquim@gmail.com>
2022-08-04 21:52:56 -0300

    User model
     * Adicionado entidade e repository de User

---
+++ auth-user/src/main/java/br/com/ofsajoaquim/authuser/domain/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.ofsajoaquim.authuser.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+	Optional<UserEntity> findByEmail(String username);
+}
