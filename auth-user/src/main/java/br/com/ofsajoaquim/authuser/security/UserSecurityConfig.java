90732beb531985a5aee0b2bcd00aeb7903bc7a0f
Luiz Felipe <luiz.sa.joaquim@gmail.com>
2022-08-05 21:31:18 -0300

    JWT user security
    	* Alterado metodo securityfilterchain
    	* Adicionado metodo para converter jwtAuthentication
    	* Adicionad spring security

--- auth-user/src/main/java/br/com/ofsajoaquim/authuser/security/UserSecurityConfig.java
+++ auth-user/src/main/java/br/com/ofsajoaquim/authuser/security/UserSecurityConfig.java
@@ -2,12 +2,22 @@ package br.com.ofsajoaquim.authuser.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,12 +29,43 @@ public class UserSecurityConfig {
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
             UserDetailsService userDetailsService) throws Exception {
-        return http.userDetailsService(userDetailsService)
+         http.userDetailsService(userDetailsService)
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .httpBasic(withDefaults())
-                .build();
+                	.csrf().disable()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+          
+                return http.build();
+    }
+    
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    	JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+    
+    	converter.setJwtGrantedAuthoritiesConverter(
+    			jwt -> {
+    				List<String> userAuthorities = jwt.getClaimAsStringList("authorities");
+    			
+    				if(userAuthorities == null)
+    					return Collections.emptyList();
+    			
+    				JwtGrantedAuthoritiesConverter scopesConverter = new JwtGrantedAuthoritiesConverter();
+    				Collection<GrantedAuthority> scopeAutorities=  scopesConverter.convert(jwt);
+    			
+    				scopeAutorities.addAll(userAuthorities.stream()
+    						.map(SimpleGrantedAuthority::new)
+    						.toList());
+    						
+    				return scopeAutorities;		
+    					
+    			}
+    			
+    	);
+    	
+    	return converter;
+    
     }
 
 }
